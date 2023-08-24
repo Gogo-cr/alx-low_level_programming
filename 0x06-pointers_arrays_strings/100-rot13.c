1a6088aa455b14b05b814a58c7015044e688d3f5
Gogo-cr <chigoziennaemeka456@gmail.com>
2023-08-23 20:54:36 -0700

    Write a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+char *rot13(char *)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			for (j = 0; j < 52; j++)
+			{
+				if (s[i] == data[j])
+				{
+					s[i] = datarot[j];
+					break;
+				}
+			}
+		}
+		return (s);
+}
